bed8822cf4dd1d58d6d05c090c66d99bd7cb52f6
dattasai <chinnidattasai@gmail.com>
2025-05-02 15:28:38 +0530

    feat: added search functionlilty and healthcheck

--- projects/empcrud/employeecrud/views.py
+++ projects/empcrud/employeecrud/views.py
@@ -12,7 +12,7 @@ def create_employee(request):
     serializer = EmployeeSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response({'message':'employee data created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -81,20 +81,6 @@ def search_employees(request):
 @api_view(['GET'])
 def api_status(request):
     return Response({'status': 'API is running successfully!'}, status=status.HTTP_200_OK)
-
-@api_view(['GET'])
-def filter_employees_by_position(request):
-    position = request.query_params.get('position', None)
-
-    if position:
-        # Filter employees by position
-        employees = Employee.objects.filter(position__iexact=position)
-        serializer = EmployeeSerializer(employees, many=True)
-        return Response(serializer.data)
-    else:
-        return Response({'error': 'Please provide a position to filter by'}, status=status.HTTP_400_BAD_REQUEST)
-
-
 
 
 @api_view(['GET'])
